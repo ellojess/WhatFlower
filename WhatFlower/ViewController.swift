7381d7318847f43258d4f4c512ad0defd172a88b
Jessica Trinh <jtjessicatrinh@gmail.com>
2019-06-04 14:23:43 -0700

    Flower recognition app

---
+++ WhatFlower/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  WhatFlower
+//
+//  Created by Bo on 6/3/19.
+//  Copyright © 2019 Jessica Trinh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    let imagePicker = UIImagePickerController()
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .camera
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let userPickedImage = info[UIImagePickerController.InfoKey.originalImage]
+        
+        imageView.image = userPickedImage as? UIImage
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+}
+
